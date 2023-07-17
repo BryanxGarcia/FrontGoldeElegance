2565213fba1ee3e0d7342192362779e2d8720e9f
Cristopher Bryan Bautista Garcia <92136012+BryanxGarcia@users.noreply.github.com>
2023-07-17 00:32:30 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-    serverUrl: 'http://apigolden.somee.com',
+    serverUrl: 'https://apigolden.somee.com',
     clientIDPayPal: 'AUr8hLgJvoCorMr6jUdmZ6E1bHFp_D1DFarWLMgq6oYw5LtaShrquGBNjR7rNR2u4JX31u_x2iyBIfiF'
   };
   
