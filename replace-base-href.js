74bca40d645f8c84191ef883054d6667e881b0a0
Cristopher Bryan Bautista Garcia <92136012+BryanxGarcia@users.noreply.github.com>
2023-07-17 00:20:57 -0500

    Update replace-base-href.js

--- replace-base-href.js
+++ replace-base-href.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const filePath = './dist/vistas-angular/index.html'; // Ruta al archivo index.html generado en la carpeta de distribución
 
 const fileContent = fs.readFileSync(filePath, 'utf8');
-const updatedContent = fileContent.replace('<base href="/">', '<base href="hhttps://BryanxGarcia.github.io/FrontGoldeElegance/">');
+const updatedContent = fileContent.replace('<base href="/">', '<base href="https://BryanxGarcia.github.io/FrontGoldeElegance/">');
 
 fs.writeFileSync(filePath, updatedContent, 'utf8');
